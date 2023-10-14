9c97584e45d7e0e5a6982db846229963d16cb9ce
ayasgo <1ayasgo@gmail.com>
2023-10-15 00:33:19 +0100

    v2

--- data_ml/data_cleaning/main.js
+++ data_ml/data_cleaning/main.js
@@ -1,18 +1,29 @@
 const canvas = document.createElement('canvas');
+const container = document.getElementById('container')
 const ctx = canvas.getContext("2d");
 canvas.width = canvas.height = 400;
+imgsToRemove = []
 
-function download_image(description, paths) {
+function download_image(paths, name) {
     ctx.fillStyle = 'white'
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     let shape = new Shape(paths);
     shape.draw(ctx);
     data = canvas.toDataURL('image/png');
-    a = document.createElement('a');
-    a.href = data;
-    a.download = description + '.png';
-    a.click()
+    img = document.createElement('img');
+    img.addEventListener('click', () => {
+        if (!(name in imgsToRemove)) {
+            imgsToRemove.push(name);
+            this.classList.add("redimg");
+        } else {
+             imgsToRemove.push(name);
+             this.classList.add("redimg");
+        }
+    })
+    img.src = data;
+    container.appendChild(img)
 }
+download_image(car['1'], )
 
 
 
