63819c220a960430e078a416a68025214ab740f3
ayasgo <1ayasgo@gmail.com>
2023-10-14 15:43:42 +0100

    v2

---
+++ version2/static/main.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+canvas.width = canvas.height = 500;
+let shape = new Shape();
+const   undo = document.getElementById("undo"),
+        save = document.getElementById("save"),
+        reset = document.getElementById("reset");
+
+animate();
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    shape.update(canvas, undo, reset, save);
+    shape.draw(ctx);
+    requestAnimationFrame(animate);
+};
